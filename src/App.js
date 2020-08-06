f54348c34c232fdd27ff68a611ac09fbe3b24059
Junaid Ali <junaidnitsri786@gmail.com>
2020-08-06 21:36:07 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+const os = require('os');
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to new world</h1>
+    </div>
+  );
+}
+
+export default App;
